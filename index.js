170620534330a05a812f10e2d33cadf15836a685
srivastavatarunima-max <srivastava.tarunima@gmail.com>
2026-06-07 14:08:07 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello! Your Node.js app is running 🚀");
+});
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
