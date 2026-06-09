e944df2bba5a56ff23ef130170883ee2f7d2d274
srivastavatarunima-max <srivastava.tarunima@gmail.com>
2026-06-10 02:32:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -364,28 +364,12 @@ app.get("/hello-test", (req, res) => {
 
 });
 
-app.get("/scrape-test", async (req, res) => {
+app.get("/scrape-test", (req, res) => {
 
-  try {
-
-    const response = await axios.get(
-      "https://www.michaelpage.ae/jobs"
-    );
-
-    res.json({
-      success: true,
-      length: response.data.length,
-      preview: response.data.substring(0, 500)
-    });
-
-  } catch (error) {
-
-    res.status(500).json({
-      success: false,
-      error: error.message
-    });
-
-  }
+  res.json({
+    success: true,
+    message: "scrape test route working"
+  });
 
 });
 
