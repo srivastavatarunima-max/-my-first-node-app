a749ce80a0d9b1c2f8b6c4197b3fa72ad69072f9
srivastavatarunima-max <srivastava.tarunima@gmail.com>
2026-06-10 00:39:11 +0530

    Created first scraper test

--- scraper.js
+++ scraper.js
@@ -1,1 +1,23 @@
-console.log("Scrapper Ready");
+const axios = require("axios");
+
+async function testScraper() {
+
+  try {
+
+    const response = await axios.get(
+      "https://www.michaelpage.ae/jobs"
+    );
+
+    console.log(
+      response.data.substring(0, 500)
+    );
+
+  } catch (error) {
+
+    console.log(error.message);
+
+  }
+
+}
+
+testScraper();
