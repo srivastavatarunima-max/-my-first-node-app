4b3a3aa23297de35b399c6d4fa1fb0537821cf0d
srivastavatarunima-max <srivastava.tarunima@gmail.com>
2026-06-14 17:49:53 +0530

    Add Google Drive authentication

--- index.js
+++ index.js
@@ -10,6 +10,21 @@ const { google } = require("googleapis");
 const PDFDocument = require("pdfkit");
 
 dotenv.config();
+const serviceAccount = JSON.parse(
+  process.env.GOOGLE_SERVICE_ACCOUNT
+);
+
+const auth = new google.auth.GoogleAuth({
+  credentials: serviceAccount,
+  scopes: [
+    "https://www.googleapis.com/auth/drive"
+  ]
+});
+
+const drive = google.drive({
+  version: "v3",
+  auth
+});
 
 const app = express();
 
